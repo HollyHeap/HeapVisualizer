495c289b82de184fd8c1b15308638bf63467197d
HollyHeap <143168002+HollyHeap@users.noreply.github.com>
2023-08-25 03:56:41 -0400

    Refactoring and code cleanup.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function validateInput(input) {
-    // TODO: Remove this before going live - Test Flag: CISK{HeapMemoryM1splace}
     if (typeof input !== 'number') {
         alert('Please enter a valid number.');
         return false;
