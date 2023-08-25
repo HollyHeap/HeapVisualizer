6647385a17735a1da73f0e17071b97aaf950c1b1
HollyHeap <143168002+HollyHeap@users.noreply.github.com>
2023-08-25 03:57:48 -0400

    Final touches and performance optimization.

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ function validateInput(input) {
     }
     return true;
 }
+
+const colors = {
+    used: 'red',
+    free: 'green'
+};
+
+function drawHeapSegment(x, y, status) {
+    ctx.fillStyle = colors[status];
+    ctx.fillRect(x, y, 50, 50);
+}
+
+function drawHeap() {
+    // Enhanced drawing of heap structure with color codes
+    for (let i = 0; i < 10; i++) {
+        drawHeapSegment(i * 55, 50, i % 2 === 0 ? 'used' : 'free');
+    }
+}
+
+drawHeap();
